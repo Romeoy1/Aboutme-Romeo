3ea2977e62ee80e63c8ce6fbb58c15b902b6a0d3
Romeoy1 <149593721+Romeoy1@users.noreply.github.com>
2023-11-06 19:12:35 -0600

    added java

--- index.js
+++ index.js
@@ -1,2 +1,82 @@
-// VAR, LET, CONST
-let x = 0; // mut
+// VARIABLES - containers for data used for later
+
+//let - are mutable / change
+let number = 15;
+
+number = 10;
+
+console.log(number);
+
+//const - immutable/connot change
+const name = "Romeo";
+
+// name = "Mai";
+
+//var - just like let
+// arent going to use
+
+// variable declarations
+
+var number2 = 0;
+
+number2 = 10;
+
+// DATA TYPES
+
+//  Number/Interger
+
+let x = 2;
+
+let y = 2.5;
+
+let z = x + y;
+
+let b = 1000000;
+
+console.log(2 ** 4);
+
+// String sequnce of characters
+
+// ' ', " ", ` `
+
+const name2 = "goodmorning";
+
+// concatenation
+
+let sentence = "hi";
+let sentence2 = "bye";
+let sentence3 = sentence + " " + sentence2;
+// ${} - tempplate literal
+let sentence4 = `Hi my name is Romeo. ${sentence2}`;
+console.log(sentence4);
+
+let o = 200;
+
+let s = `${o}`;
+
+let num = "20,000";
+
+let num2 = "20";
+
+// let num = '20' always going to be a string
+
+console.log(num + num2);
+
+const numb3 = 10;
+
+// console.log(numb3); prints to the console
+
+// Booleans
+
+// Truthy - anything with value
+// 1
+// 'Great'
+// true
+// Falsy - undefined; no value
+//0
+// ''
+// false
+// undefined
+// null
+
+console.log(Boolean("1000"));
